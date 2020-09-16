63dd52a05d0995444d63aa192abc69a70e831e5d
Cory Loken <cory@me.com>
2020-09-15 22:21:47 -0400

    Get ranges working

--- app/routes/contacts.js
+++ app/routes/contacts.js
@@ -1,23 +1,29 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { get } from '@ember/object';
 
 export default class ContactsRoute extends Route {
   @service ellaSparse;
   @service store;
 
+  range = {
+    length: 50,
+    start: 1
+  }
+
   async model(params) {
     return this.ellaSparse.array(async (range = {}, query = {}) => {
-      // TODO: How does range get set?
+      // Use get as we need the values in the scope of the sparse array.
       let page = {
-        size: 50, //get(range, 'length') || 10,
-        number: this.range?.start || 1
+        size: get(range, 'length') || 10,
+        number: get(range, 'start') || 1
       };
-
       query.include = 'phone-numbers';
       query.page = page;
       // Return a Promise that resolves with the array of fetched data
       // and the total available records
       let result = await this.store.query('contact', query);
+
       return {
         data: result,
         // TODO: do a calculation based on link.last
